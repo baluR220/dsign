17c6e83514d1a6a47bb4f0e22b097e893ab90c8c
baluR220 <1qasw23edfr45gt6@gmail.com>
2022-04-22 12:21:12 +0700

    update files

---
+++ src/conf.py
@@ -0,0 +1,14 @@
+# Connfig options
+
+BG_COLOR = '#2e3436'
+
+BTN_COLOR = '#babdb6'
+
+# Fade timeout in ms. Set to 0 to disable fade
+FADE_TO = 5000
+
+# Fade in speed - amount of alpha substructed every 50 ms
+FADE_IN_SP = 0.05
+
+# Fade out speed - amount of alpha added every 50 ms
+FADE_OUT_SP = 0.2
--- src/player.py
+++ src/player.py
@@ -1,9 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 
-
-bg_color = '#2e3436'
-btn_color = '#babdb6'
+import conf
 
 
 class Player(tk.Tk):
@@ -11,14 +9,14 @@ class Player(tk.Tk):
     def __init__(self):
         root_win = tk.Tk()
         root_win.geometry("600x600+0+0")
-        root_win.wm_attributes("-fullscreen", True)
+        #root_win.wm_attributes("-fullscreen", True)
         root_win.update()
 
         ttk.Style().configure(
-            "Custom.TFrame", background=bg_color
+            "Custom.TFrame", background=conf.BG_COLOR
         )
         ttk.Style().configure(
-            "Custom.TButton", background=btn_color
+            "Custom.TButton", background=conf.BTN_COLOR
         )
 
         fw_width = int(root_win.winfo_width() * 0.8)
@@ -30,20 +28,22 @@ class Player(tk.Tk):
         fade_win.overrideredirect(True)
         fade_win.focus_set()
         fade_win.lift(aboveThis=root_win)
-        fade_win.wm_attributes("-alpha", 0.0)
+        fade_win.wm_attributes("-alpha", 1)
 
         main_frame = ttk.Frame(
             root_win, width=root_win.winfo_width(),
             height=root_win.winfo_height(), style='Custom.TFrame')
         main_frame.pack()
-        main_frame.bind(
-            '<Button-1>',
-            lambda event: self.fade_out(event, fade_win)
-        )
-        fade_win.bind(
-            '<Button-1>',
-            lambda event: self.fade_out(event, fade_win)
-        )
+        if not conf.FADE_TO == 0:
+            fade_win.wm_attributes("-alpha", 0.0)
+            main_frame.bind(
+                '<Button-1>',
+                lambda event: self.fade_out(event, fade_win)
+            )
+            fade_win.bind(
+                '<Button-1>',
+                lambda event: self.fade_out(event, fade_win)
+            )
         b_frame = ttk.Frame(fade_win, style='Custom.TFrame')
         b_frame.pack(fill=tk.X)
         left_b = ttk.Button(b_frame, text='Left', style='Custom.TButton')
@@ -58,15 +58,15 @@ class Player(tk.Tk):
     def fade_in(self, event, frame):
         alpha = frame.wm_attributes("-alpha")
         if alpha > 0:
-            alpha -= 0.05
+            alpha -= conf.FADE_IN_SP
             frame.wm_attributes("-alpha", alpha)
             self.fiap = frame.after(50, self.fade_in, event, frame)
 
     def fade_in_with_timer(self, event, frame, wait=True):
         if wait:
             wait = False
-            self.fiwtap = frame.after(5000, self.fade_in_with_timer, event,
-                                      frame, wait)
+            self.fiwtap = frame.after(conf.FADE_TO, self.fade_in_with_timer,
+                                      event, frame, wait)
         else:
             wait = True
             self.fade_in(event, frame)
@@ -78,7 +78,7 @@ class Player(tk.Tk):
             frame.after_cancel(self.fiwtap)
         alpha = frame.wm_attributes("-alpha")
         if alpha < 1:
-            alpha += 0.2
+            alpha += conf.FADE_OUT_SP
             frame.wm_attributes("-alpha", alpha)
             frame.after(50, self.fade_out, event, frame)
         else:
